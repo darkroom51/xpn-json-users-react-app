b33da41f46a0091ca8823a70c2820d79055e72c9
Waldek Fryc <waldemar.fryc@gmail.com>
2018-02-12 14:21:25 +0100

    created componentns and basic redux structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import UsersList from './components/UsersList'
+import UsersDetails from './components/UsersDetails'
+
+import {Provider} from 'react-redux'
+import store from './store'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-     hello world
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div>
+                        <Route path="/" exact={true} component={UsersList}/>
+                        <Route path="/users-details/:uid/" component={UsersDetails}/>
+                    </div>
+                </BrowserRouter>
+            </Provider>
+        );
+    }
 }
 
 export default App;
